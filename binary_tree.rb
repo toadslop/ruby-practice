9bea30ea8a510f51e632a64bf34b73fa72fa58e4
Brian Heise <bnheise@gmail.com>
2020-09-11 20:50:56 +0900

    created insert function

--- binary_tree.rb
+++ binary_tree.rb
@@ -14,17 +14,52 @@ class BinaryTree
     def initialize(head = nil)
       @head = head
     end
+
+    def in_order(temp = @head)
+      unless temp
+        return
+      end
+
+      in_order(temp.left)
+      puts temp.data.to_s + " "
+      in_order(temp.right)
+
+    end
+
+    def insert(data)
+      queue = []
+      queue.append(@head)
+
+      while queue.length
+        temp = queue[0]
+        queue = queue.drop(1)
+
+        unless temp.left
+          temp.left = TreeNode.new(data)
+          break
+        else
+          queue.append(temp.left)
+        end
+
+        unless temp.right
+          temp.right = TreeNode.new(data)
+          break
+        else
+          queue.append(temp.right)
+        end
+      end
+    end
 end
 
-# tree = BinaryTree.new
-# p tree
-# p tree.head
+tree = BinaryTree.new
 
-# node1 = TreeNode.new(1)
-# tree.head = node1
-# node2 = TreeNode.new(2)
-# node3 = TreeNode.new(3)
-# node1.left = node2
-# node1.right = node3
-
-# p tree
+tree.head = TreeNode.new(10)
+tree.head.left = TreeNode.new(11)
+tree.head.left.left = TreeNode.new(7)
+tree.head.right = TreeNode.new(9)
+tree.head.right.left = TreeNode.new(15)
+tree.head.right.right = TreeNode.new(8)
+tree.in_order
+tree.insert(12)
+puts ""
+tree.in_order
