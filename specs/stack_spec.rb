f9a333c0d5cc40ddd55b9742add75b496b4728a2
Brian Heise <bnheise@gmail.com>
2020-09-26 18:38:12 +0900

    added check on stack length

--- specs/stack_spec.rb
+++ specs/stack_spec.rb
@@ -2,9 +2,13 @@ require './stack'
 
 RSpec.describe Stack do
   context "when initializing" do
+    stack = Stack.new
     it "has no data" do
-      stack = Stack.new
       expect(stack.data).to eq([])
+    end
+
+    it "has length of 0" do
+      expect(stack.length).to eq(0)
     end
   end
 end
